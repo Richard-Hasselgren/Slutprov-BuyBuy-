76d0b6fe60ebeef392d8f82d1afa2d5458821401
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2015-04-27 09:42:04 +0200

    First seed, USER.

--- app/db/seed.rb
+++ app/db/seed.rb
@@ -1,7 +1,12 @@
 class Seeder
 
   def self.seed!
+    users
+  end
 
+  def self.users
+    User.create(uname: 'banan', pass: 'hejhej')
+    User.create(uname: 'banan2', pass: 'hejhej2')
   end
 
 end
