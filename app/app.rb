99e30d50d63a7c2a8606bc0d048dfab302f29ff9
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2015-05-07 09:57:47 +0200

    Login & sign up
    Login fungerar och skickar dig till Welcome page för user.
    sign up är på väg att skapas.

--- app/app.rb
+++ app/app.rb
@@ -6,7 +6,30 @@ class App < Sinatra::Base
   end
 
   get '/login' do
+    slim :login
+  end
 
+  post '/login' do
+    user = User.first(uname: params['uname'])
+    if user && user.pass == params['pass']
+      session[:user] = user.id
+      redirect '/welcome'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/sign' do
+    slim :sign
+  end
+
+  get '/welcome' do
+    if session[:user]
+      @user = User.get(session[:user])
+      slim :welcome
+    else
+      redirect '/'
+    end
   end
 
 
