0914d37582ddcb7d54620a637d239b99903231c2
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2015-05-08 12:05:01 +0200

    passande namn.
    Har byt list.slim till list_item.slim

--- app/app.rb
+++ app/app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
 
   post '/login' do
-    user = User.first(uname: params['uname'])
+    user = User.first(uname: params['uname'].downcase)
     if user && user.pass == params['pass']
       session[:user] = user.id
       redirect '/welcome'
@@ -27,7 +27,7 @@ class App < Sinatra::Base
     if params['pass'].length <= 3
       redirect '/sign'
     end
-    sign = User.create(uname: params['uname'], pass: params['pass'])
+    sign = User.create(uname: params['uname'].downcase, pass: params['pass'])
     if sign && sign.valid?
       session[:user] = sign.id
       redirect '/welcome'
@@ -46,9 +46,16 @@ class App < Sinatra::Base
     end
   end
 
-  get '/list' do
-    slim :list
+  get '/list_item' do
+    if session[:user]
+      @user = User.get(session[:user])
+    else
+      redirect '/'
+    end
+    slim :list_item
   end
+
+  #post
 
 
 end
